795647713dbf93d8038fc153be67297b8e2ebda6
vivevincere <s.vincent@live.com.sg>
2020-07-05 07:04:37 +0800

    formatting

--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -1,75 +1,75 @@
 package main
 
 import (
-    "encoding/hex"
-    "fmt"
+	"encoding/hex"
+	"fmt"
 
-    "github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
 const (
-    indexName = "GooseEvent"
+	indexName = "GooseEvent"
 )
 
 // DltAudit provides functions for logging GOOSE events to the ledger
 type DltAudit struct {
-    contractapi.Contract
+	contractapi.Contract
 }
 
 // InitLedger provides initialization
 func (d *DltAudit) InitLedger(ctx contractapi.TransactionContextInterface) error {
-    return nil
+	return nil
 }
 
 // LogEvent will log a new GOOSE event to the ledger
 func (d *DltAudit) LogEvent(ctx contractapi.TransactionContextInterface, id string, timestamp string, goosePacket string) error {
-    key, err := ctx.GetStub().CreateCompositeKey(indexName, []string{id, string(timestamp)})
-    if err != nil {
-        return fmt.Errorf("Error creating composite key for GooseEvent")
-    }
+	key, err := ctx.GetStub().CreateCompositeKey(indexName, []string{id, string(timestamp)})
+	if err != nil {
+		return fmt.Errorf("Error creating composite key for GooseEvent")
+	}
 
-    packetBytes, err := hex.DecodeString(goosePacket)
-    if err != nil {
-        return fmt.Errorf("Error decoding the GOOSE packet (expected valid hex string)")
-    }
+	packetBytes, err := hex.DecodeString(goosePacket)
+	if err != nil {
+		return fmt.Errorf("Error decoding the GOOSE packet (expected valid hex string)")
+	}
 
-    err = ctx.GetStub().PutState(key, packetBytes)
-    if err != nil {
-        return fmt.Errorf("Error putting key '%s' and value '%s'", key, packetBytes)
-    }
+	err = ctx.GetStub().PutState(key, packetBytes)
+	if err != nil {
+		return fmt.Errorf("Error putting key '%s' and value '%s'", key, packetBytes)
+	}
 
-    return ctx.GetStub().SetEvent("logEvent", packetBytes)
+	return ctx.GetStub().SetEvent("logEvent", packetBytes)
 }
 
 // QueryEvent gets the event for a given IED ID and timestamp
 func (d *DltAudit) QueryEvent(ctx contractapi.TransactionContextInterface, id string, timestamp string) (string, error) {
-    key, err := ctx.GetStub().CreateCompositeKey(indexName, []string{id, string(timestamp)})
-    if err != nil {
-        return "", fmt.Errorf("Error creating composite key for GooseEvent")
-    }
+	key, err := ctx.GetStub().CreateCompositeKey(indexName, []string{id, string(timestamp)})
+	if err != nil {
+		return "", fmt.Errorf("Error creating composite key for GooseEvent")
+	}
 
-    eventAsBytes, err := ctx.GetStub().GetState(key)
+	eventAsBytes, err := ctx.GetStub().GetState(key)
 
-    if err != nil {
-        return "", fmt.Errorf("Failed to read: %s", err.Error())
-    }
+	if err != nil {
+		return "", fmt.Errorf("Failed to read: %s", err.Error())
+	}
 
-    if eventAsBytes == nil {
-        return "", fmt.Errorf("%s does not exist", id)
-    }
+	if eventAsBytes == nil {
+		return "", fmt.Errorf("%s does not exist", id)
+	}
 
-    return hex.EncodeToString(eventAsBytes), nil
+	return hex.EncodeToString(eventAsBytes), nil
 }
 
-func main(){
-    dltAudit := new(DltAudit)
-    contract,err := contractapi.NewChaincode(dltAudit)
+func main() {
+	dltAudit := new(DltAudit)
+	contract, err := contractapi.NewChaincode(dltAudit)
 
-    if err != nil {
-        panic(err.Error())
-    }
+	if err != nil {
+		panic(err.Error())
+	}
 
-    if err := contract.Start(); err != nil{
-        panic(err.Error())
-    }
+	if err := contract.Start(); err != nil {
+		panic(err.Error())
+	}
 }
--- main.go
+++ main.go
@@ -1,26 +1,25 @@
 package main
 
-import(
+import (
 	"fmt"
 
 	"github.com/vivevincere/goosefabric"
-	"time"
 	"log"
-
+	"time"
 )
 
-func main(){
+func main() {
 
 	objects := goosefabric.Init("./config.yaml", "mychannel", "Admin", "Org1")
-	err := objects.LogEvent("base", "faucet","1234", "6d61697961686565")
-	if err != nil{
+	err := objects.LogEvent("base", "faucet", "1234", "6d61697961686565")
+	if err != nil {
 		log.Fatal("LogEvent failed: %v", err)
 	}
 	time.Sleep(2 * time.Second)
-	k, err := objects.Get("base", "faucet","1234")
-	if err != nil{
+	k, err := objects.Get("base", "faucet", "1234")
+	if err != nil {
 		log.Fatal("QueryEvent failed: %v", err)
 	}
-	fmt.Printf(k)	
+	fmt.Printf(k)
 
 }
